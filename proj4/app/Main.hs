f73909413b20c87d0a6bf000189ea97fb494bee8
shridhay <47260704+shridhay@users.noreply.github.com>
2025-12-17 01:13:49 -0500

    Update Main.hs

--- proj4/app/Main.hs
+++ proj4/app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Verifier.Verify (verify, VerifyResult(..), Violation(..))
+import Verifier.Verify (verify)
 import System.Environment 
 import System.IO.Error (catchIOError)
 
@@ -9,20 +9,13 @@ main = do
     as <- getArgs
     case as of 
       [filename, intstr] -> do
-        progstr <- catchIOError (readFile filename)
+        progstr <- catchIOError (readFile filename) (\_ -> error "Could not read file")
         let n = read intstr :: Int
         result <- verify progstr n
+        print result  
       _ ->
-      error "Usage: see <program.imp> <loop-bound>"
+        error "Usage: see <program.imp> <loop-bound>"
 
-
-
-    -- prog <- readFile (head as)
-    -- result <- catchIOError (verify prog) (return . Unknown . show)
-    -- case result of
-    --   Verified -> putStrLn "Verified"
-    --   NotVerified -> putStrLn "Not verified"
-    --   Unknown msg -> putStrLn ("Verifier returned unknown: " ++ msg)
 -- main :: IO ()
 -- main = do
 --     as <- getArgs
