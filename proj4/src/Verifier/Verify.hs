6e830428c7497db26d1b9ff4cdacdc6a0f189fa4
shridhay <47260704+shridhay@users.noreply.github.com>
2025-12-17 14:41:26 -0500

    Refactor verify function and clean up imports
    Removed unused imports and the Result type. Updated the verify function to return a Maybe String instead of Result.

--- proj4/src/Verifier/Verify.hs
+++ proj4/src/Verifier/Verify.hs
@@ -1,27 +1,21 @@
-module Verifier.Verify (Result(..), verify) where
+module Verifier.Verify (verify) where
 
 import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Lazy.Char8 as LC
 
-import Verifier.GC
-import Parser.Parser
-
 import SMTLIB.Backends (QueuingFlag (..), command, command_, initSolver)
 import qualified SMTLIB.Backends.Z3 as Z3
 
-data Result = Verified | NotVerified | Unknown String
-  deriving (Eq, Show)
-
-verify :: String -> Int -> IO Result
-verify progString n = do 
-  case (parseProg progString) of 
-    Nothing -> return (Unknown "Parsing failed")
-    Just p -> do 
-      let z3string = getZ3String p
-      Z3.with Z3.defaultConfig $ \handle -> do
-        solver <- initSolver Queuing (Z3.toBackend handle)
-        res <- command solver (B.stringUtf8 z3string)
-        case (LC.unpack res) of
-          "sat\n" -> return NotVerified
-          "unsat\n" -> return Verified
-          s -> return (Unknown s)
+verify :: String -> IO (Maybe String)
+verify z3string = do
+  Z3.with Z3.defaultConfig $ \handle -> do
+    solver <- initSolver Queuing (Z3.toBackend handle)
+    res <- command solver (B.stringUtf8 z3string)
+    case (LC.unpack res) of
+      "sat\n" -> do
+        model <- command solver (B.stringUtf8 "(get-model)")
+        return (Just (LC.unpack model))
+      "unsat\n" -> 
+        return Nothing
+      other -> 
+        return (Just ("Unknown Z3 result: " ++ other))
